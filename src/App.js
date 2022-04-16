10f9c0de574beaee0a4457bf27bee8b82f81a007
Sujeet Padhi <sujitclick7.sp@gmail.com>
2022-04-16 22:13:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+const App = () => {
+  const [valueA,setValueA] = useState(0);
+  const [valueB,setValueB] = useState(0);
 
-function App() {
+  const [operations,setOperations] = useState('+');
+  const [result,setResult] = useState(0);
+  
+  const handleSubmit = () => {
+    if(operations==='+'){
+      setResult(parseInt(valueA)+parseInt(valueB));
+    }
+  
+    if(operations==='*'){
+    setResult(valueA*valueB);
+    }
+  
+    if(operations==='-'){
+    setResult(valueA-valueB);
+    }
+  
+    if(operations==='/'){
+    setResult(valueA/valueB);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className='app'>
+      <h2>Calculator app</h2>
+      <h3 >Result: {result}</h3>
+      <div>
+        <input type="text" className="form-control col-4" value={valueA} onChange={(e)=>setValueA(e.target.value)}></input><br/>
+        <h1 styles={{marginLeft:'40%'}}>{operations}</h1>
+        <input type="text" className="form-control col-4" value={valueB} onChange={(e)=>setValueB(e.target.value)}></input><br/>
+        <button styles={{marginLeft:'10%'}} onClick={handleSubmit} className="btn btn-info" >Calculate</button>
+      </div>
+      <div>
+        <button onClick={()=>setOperations('+')} className="btn btn-primary " >+</button>
+        <button onClick={()=>setOperations('-')} className="btn btn-primary" >-</button>
+        <button onClick={()=>setOperations('*')} className="btn btn-primary" >*</button>
+        <button onClick={()=>setOperations('/')} className="btn btn-primary" >/</button>
+      </div>
     </div>
-  );
+    </div>
+  )
 }
 
 export default App;
